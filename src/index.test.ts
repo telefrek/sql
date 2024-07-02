88937cd60000b79cb7415a55de5a3e46126b2b7c
Nathan Northcutt <40191617+nathannorthcutt@users.noreply.github.com>
2024-07-02 07:32:24 +0200

    Part 2 - Database Schemas (#4)
    final state at the end of part-2 for the SQL series, will start working on part-3 with some basic queries and test database schema

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,56 @@
-import { GREETING } from "./index.js"
+import { TEST_DATABASE } from "./testUtils.js"
+import { SQLBuiltinTypes } from "./types.js"
 
-describe("This is a dummy test setup", () => {
-  it("Should run", () => {
-    expect(GREETING).not.toBeUndefined()
+describe("Schema building should create valid schemas", () => {
+  // Test to verify that our schema actually matches what we defined as types
+  it("The test database schema should match the type information", () => {
+    expect(TEST_DATABASE).not.toBeUndefined()
+    expect(TEST_DATABASE.tables).not.toBeUndefined()
+
+    // Check the order table
+    expect(TEST_DATABASE.tables.orders).not.toBeUndefined()
+
+    // Check the key
+    expect(TEST_DATABASE.tables.orders.primaryKey.column).toMatchObject([
+      "user_id",
+      "product_id",
+    ])
+
+    // Check the columns
+    expect(TEST_DATABASE.tables.orders).not.toBeUndefined()
+    expect(TEST_DATABASE.tables.orders.columns.id.type).toBe(
+      SQLBuiltinTypes.BIGINT
+    )
+    expect(TEST_DATABASE.tables.orders.columns.id.autoIncrement).toBeTruthy()
+
+    // Verify the default method is there and provides correct information
+    expect(
+      TEST_DATABASE.tables.orders.columns.order_timestamp.default
+    ).not.toBeUndefined()
+    expect(
+      typeof TEST_DATABASE.tables.orders.columns.order_timestamp.default
+    ).toBe("function")
+    const defaultProvider = TEST_DATABASE.tables.orders.columns.order_timestamp
+      .default as () => number
+    expect(defaultProvider).not.toBeUndefined()
+    expect(defaultProvider()).toBeLessThanOrEqual(Date.now())
+
+    // Just check tables and keys
+    expect(TEST_DATABASE.tables.products).not.toBeUndefined()
+    expect(TEST_DATABASE.tables.products.primaryKey).not.toBeUndefined()
+    expect(TEST_DATABASE.tables.products.primaryKey.column).toBe("id")
+
+    expect(TEST_DATABASE.tables.users).not.toBeUndefined()
+    expect(TEST_DATABASE.tables.users.primaryKey).not.toBeUndefined()
+    expect(TEST_DATABASE.tables.users.primaryKey.column).toBe("id")
+
+    expect(TEST_DATABASE.relations.orders_product_fk.reference).toBe("products")
+    expect(TEST_DATABASE.relations.orders_product_fk.target).toBe("orders")
+    expect(TEST_DATABASE.relations.orders_product_fk.targetColumns[0]).toBe(
+      "product_id"
+    )
+    expect(TEST_DATABASE.relations.orders_product_fk.referenceColumns[0]).toBe(
+      "id"
+    )
   })
 })
